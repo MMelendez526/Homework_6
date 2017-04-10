1b0122fc2b55b5618f75e8c8abdc69deb5e4c0dd
MMelendez526 <m.melendez@tcu.edu>
2017-04-10 09:13:25 -0500

    Delete Melendez_hw3_p1.py

--- Melendez_hw3_p1.py
+++
@@ -1,9 +0,0 @@
-#Computational Homework #3, Problem #1
-#Need to plot a .txt file and give it a color scheme 
-
-from pylab import imshow, show
-import matplotlib.pyplot as plt
-
-data = loadtxt("stm.txt", float)
-imshow(data)
-plt.savefig('silicon_density.eps')
