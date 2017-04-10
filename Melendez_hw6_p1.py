270ec9f5a2febee65cfa349dcf1e52afcaefa1a2
MMelendez526 <m.melendez@tcu.edu>
2017-04-10 17:44:45 -0500

    Add files via upload

---
+++ Melendez_hw6_p1.py
@@ -0,0 +1,60 @@
+from __future__ import division, print_function
+#from vpython import *
+from math import *
+import numpy as np
+from numpy import arange, pi
+from pylab import plot, xlabel, ylabel, show, title, savefig
+
+def f(r, t):
+    theta = r[0]
+    omega = r[1]
+    ftheta = omega
+    fomega = -(g/l)*sin(theta*(np.pi/180)) #solves d-omega/dt
+    return np.array([ftheta*180/np.pi, fomega], float) #Need to convert back to degrees
+
+#Constants
+g = 9.81 #gravity (m/s)
+l = 0.08 #want an 8 cm arm length, but need to convert to meters
+
+#Need to feed the initial conditions
+a = 0.0 #starting time in seconds
+b = 10 #ending time in seconds 
+N = 1000 #number of steps
+h = (b-a)/N #step-size
+
+#Set up places for answers to go, with starting initial conditions
+tpoints = arange(a, b, h)
+xpoints = []
+r = [-179, 0.0]
+
+#4th order Runge-Kutta Method
+for t in tpoints:
+    xpoints.append(r[0])
+    k1 = h*f(r, t)
+    k2 = h*f(r + 0.5*k1, t + 0.5*h)
+    k3 = h*f(r + 0.5*k2, t + 0.5*h)
+    k4 = h*f(r + k3, t + h)
+    r += (k1 + 2*k2 + 2*k3 + k4)/6
+
+plot(tpoints, xpoints)
+title('Modeling Nonlinear Pendulum with Runge-Kutta')
+xlabel('Time (seconds)')
+ylabel('Angle (degrees)')
+savefig('Pendulum_RK.eps')
+show()
+
+################    THIS PART ISN'T WORKING     ################
+################  SOMETHING WRONG WITH VPYTHON  ################
+#ball_1 = sphere(pos = vector(0,0,0), radius = 0.1)
+#ball3 = sphere(pos = vector(0,0,0), radius = 0.1)
+#pointer = arrow(pos = vector(0,0,0), axis = ball1.pos - ball3.pos, color = color.orange)
+#
+#thing = tpoints.size
+#for i in range(thing):
+#    rate(30)
+#    xdiff = xpoints[i]
+#    y = -cos(xdiff)
+#    x = -sin(xdiff)
+#    ball3.pos = vector(x,y,0)
+#    pointer.pos = vector(x,y,0)
+#    pointer.axis = ball1.pos - ball3.pos
